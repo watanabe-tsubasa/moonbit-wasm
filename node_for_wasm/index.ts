155e5185c11a6c320e5c0c8322dddf8b15aef97b
watanabe-tsubasa <t.watanabe423@gmail.com>
2026-01-01 19:13:36 +0900

    fix

--- node_for_wasm/index.ts
+++ node_for_wasm/index.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 
-const bytes = await fs.readFile("./target/wasm/release/build/moonbit_wasm.wasm");
+const bytes = await fs.readFile("../target/wasm/release/build/moonbit_wasm.wasm");
 
 const { instance } = await WebAssembly.instantiate(bytes, {});
 
